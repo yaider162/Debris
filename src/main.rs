86f4c126b537219b9d6f7517e4ffce36b3dd73e7
Yaider <yaideralejo2@gmail.com>
2026-06-02 19:04:17 -0500

    feat: first version to understand Iced and how it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{debug, widget::{Column, button, column, text}};
+
+
+fn main() -> iced::Result{
+    iced::run(Counter::update, Counter::view)
+}
+
+#[derive(Default)]
+struct Counter {
+    count: i64,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message){
+        match message {
+            Message::Decrement => {self.count-=1}
+            Message::Increment => {self.count+=1}
+            Message::Reset => {self.count=0}
+        }
+    }
+
+    fn view(&self) -> Column<Message>{
+        let counter = Counter {count:0};
+
+        let increment = button("+").on_press(Message::Increment);
+        let decrement = button("-").on_press(Message::Decrement);
+        let reset = button("RESET").on_press(Message::Reset);
+
+        let txt = text(self.count);
+
+        let interface = column![increment, txt, decrement, reset];
+
+        interface
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+    Reset,
 }
